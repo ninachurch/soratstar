ab9ae4ec8c6c8e940a69b66a59ef9189eadf7635
Nina Church <ninapearlchurch@gmail.com>
2014-08-20 11:29:54 -0700

    added navbar

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,6 +2,10 @@ require 'httparty'
 
 get '/' do
   p ENV['AWS_ACCESS_KEY_ID']
-  @res = Amazon::Ecs.item_search('ruby', :search_index => 'All')
+  @res = Amazon::Ecs.item_search('glitter', :search_index => 'All')
   erb :index
+end
+
+get '/home' do
+
 end
