1d537074c3cfc23f1f315b70ee330cfda7f8963e
Nina Church <ninapearlchurch@gmail.com>
2014-08-20 15:26:08 -0700

    fixed bug in seeder

--- db/migrate/20140819172424_create_items.rb
+++ db/migrate/20140819172424_create_items.rb
@@ -2,7 +2,7 @@ class CreateItems < ActiveRecord::Migration
   def change
     create_table :items do |t|
       t.string :name
-      t.belongs_to :crafts
+      t.belongs_to :craft
     end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,19 @@ require 'faker'
 
 @images = ["http://i.imgur.com/3KXQPSl.jpg?1", "http://imgur.com/j05J4cu", "http://imgur.com/qHPQ3QE"]
 
+##create sample themes
+4.times do
+  Theme.create(name: Faker::Commerce.product_name)
+end
+
+@themes = Theme.all
 ##create sample craft objects
 6.times do
   Craft.create(name: Faker::Commerce.product_name,
               description: Faker::Company.catch_phrase,
               source_url: Faker::Internet.url('example.com'),
               image_url: @images.sample,
-              theme: Faker::Commerce.department)
+              theme_id: @themes.sample.id)
 end
 
 ##create 5 items for each craft
@@ -20,6 +26,6 @@ crafts =  Craft.all
 crafts.each_with_index do |category, index|
   @craft_id = index + 1
   5.times do
-    Item.create(category_id: @craft_id, name: Faker::Commerce.product_name)
+    Item.create(craft_id: @craft_id, name: Faker::Commerce.product_name)
   end
 end
