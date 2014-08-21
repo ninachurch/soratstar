a3309ff2c5356163dd0e655c267a248dfb282feb
Nina Church <ninapearlchurch@gmail.com>
2014-08-21 01:09:09 -0700

    updated mvp color scheme'

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,6 +1,8 @@
 
 get '/' do
-  erb :index
+  @crafts = crafts
+  @crafts_array = @crafts.to_a
+  erb :home
 end
 
 
@@ -12,18 +14,16 @@ end
 
 get '/craft/:id' do
   @craft = crafts.find_by_id(params[:id])
-  @items = @craft.items
+  items = @craft.items
 
   @res = {}
 
-  @items.each do |item|
-    p ENV['AWS_ACCESS_KEY_ID']
-    all_res = Amazon::Ecs.item_search("#{item.name}", :item_page => 1, :search_index => 'All')
+  items.each do |item|
+    ENV['AWS_ACCESS_KEY_ID']
+    all_res = Amazon::Ecs.item_search("#{item.name}", :search_index => 'All')
     @top_res = all_res.items.first
     @res["#{item.name}"] = @top_res
-
   end
-
-    p @res
+  p @res
   erb :profile
 end
