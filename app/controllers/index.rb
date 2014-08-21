5bf6ef415937ccea7ba3630c284acfcf3d856b30
Nina Church <ninapearlchurch@gmail.com>
2014-08-20 22:27:28 -0700

    formatted amazon template

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -14,6 +14,9 @@ get '/craft/:id' do
   @craft = crafts.find_by_id(params[:id])
 
   p ENV['AWS_ACCESS_KEY_ID']
-  @res = Amazon::Ecs.item_search('glitter', :search_index => 'All')
+  all_res = Amazon::Ecs.item_search('puppies', :item_page => 1, :search_index => 'All')
+
+  @res = all_res.items[0..3]
+
   erb :profile
 end
